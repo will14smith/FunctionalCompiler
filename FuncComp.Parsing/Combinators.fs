8a7b01bb52c538381e95065bb41b036d3ce9a9af
Will Smith <will@toxon.co.uk>
2020-11-15 01:36:53 +0000

    Don't rebuild the parsers every time...

--- FuncComp.Parsing/Combinators.fs
+++ FuncComp.Parsing/Combinators.fs
@@ -1,39 +1,99 @@
 ﻿module FuncComp.Parsing.Combinators
 
-type Parser<'a> = Token list -> ('a * Token list) seq
-
-let sat pred tokens =
-    match tokens with
-    | token :: rest -> if pred token then Seq.singleton (token, rest) else Seq.empty
-    | _ -> Seq.empty
-
-let alt (p1 : Parser<'a>) (p2 : Parser<'a>) tokens = Seq.append (p1 tokens) (p2 tokens)
-
-let altM ( ps : Parser<'a> list) (tokens : Token List) = ps |> Seq.map (fun a -> a tokens) |> Seq.concat
-
-let bind (f : 'a -> 'b -> 'c)  (p1 : Parser<'a>) (p2 : Parser<'b>) tokens = seq {
-    for (v1, tokens) in p1 tokens do
-    for (v2, tokens) in p2 tokens do
-        (f v1 v2, tokens)
-}
-let bind3 (f : 'a -> 'b -> 'c -> 'd)  (p1 : Parser<'a>) (p2 : Parser<'b>) (p3 : Parser<'c>) tokens = seq {
-    for (v1, tokens) in p1 tokens do
-    for (v2, tokens) in p2 tokens do
-    for (v3, tokens) in p3 tokens do
-        (f v1 v2 v3, tokens)
-}
-let bind4 (f : 'a -> 'b -> 'c -> 'd -> 'e)  (p1 : Parser<'a>) (p2 : Parser<'b>) (p3 : Parser<'c>) (p4 : Parser<'d>) tokens = seq {
-    for (v1, tokens) in p1 tokens do
-    for (v2, tokens) in p2 tokens do
-    for (v3, tokens) in p3 tokens do
-    for (v4, tokens) in p4 tokens do
-        (f v1 v2 v3 v4, tokens)
+type ParserLabel = string
+type ParserError = string
+type Parser<'T> = {
+    parse: Token list -> Result<'T * Token list, ParserLabel * ParserError>
+    label: ParserLabel
 }
 
-let apply (p : Parser<'a>) (f : 'a -> 'b) tokens = p tokens |> Seq.map (fun (a, ts) -> (f a, ts))
+let run (parser: Parser<'a>) = parser.parse
 
-let empty value tokens = Seq.singleton (value, tokens)
-let rec zeroOrMore p = alt (oneOrMore p) (empty Seq.empty)
-    and oneOrMore p = bind (fun a b -> Seq.append [a] b) p (zeroOrMore p)
+let createParserForwardedToRef<'a> () =
+    let dummyParser=
+        let inner _ : Result<'a * Token list, ParserLabel * ParserError> = failwith "unfixed forwarded parser"
+        { parse = inner; label = "unknown" }
 
-let oneOrMoreWithSep (p : Parser<'a>) (s : Parser<'b>) tokens = bind (fun a b -> Seq.append [a] b) p (zeroOrMore <| bind (fun _ b -> b) s p) tokens
+    let parserRef = ref dummyParser
+
+    let inner input =
+        run !parserRef input
+    let wrapperParser = {parse = inner; label = "unknown"}
+
+    wrapperParser, parserRef
+
+let sat (pred : (Token -> bool)) label : Parser<Token> =
+    let inner input =
+        match input with
+        | head::rest when pred head -> Ok (head, rest)
+        | _ -> Error (label, "no match")
+
+    { parse = inner; label = label }
+
+let bind (combine : 'a -> 'b -> 'c) (p1 : Parser<'a>) (p2 : Parser<'b>) : Parser<'c> =
+    let inner input =
+        run p1 input |> Result.bind (fun (v1, input) -> run p2 input |> Result.map (fun (v2, input) -> (combine v1 v2, input)))
+
+    { parse = inner; label = "unknown" }
+
+let bind3 (f : 'a -> 'b -> 'c -> 'd)  (p1 : Parser<'a>) (p2 : Parser<'b>) (p3 : Parser<'c>) : Parser<'d> = bind (fun f c -> f c) (bind f p1 p2) p3
+let bind4 (f : 'a -> 'b -> 'c -> 'd -> 'e)  (p1 : Parser<'a>) (p2 : Parser<'b>) (p3 : Parser<'c>) (p4 : Parser<'d>) : Parser<'e> = bind (fun f d -> f d) (bind (fun f c -> f c) (bind f p1 p2) p3) p4
+
+let alt (p1 : Parser<'a>) (p2: Parser<'a>) : Parser<'a> =
+    let inner input =
+        match run p1 input with
+        | Ok result -> Ok result
+        | Error _ -> run p2 input
+
+    { parse = inner; label = "unknown" }
+
+let choice ps = List.reduce alt ps
+
+let mapP (f : 'a -> 'b) (p : Parser<'a>) : Parser<'b> =
+    let inner input =
+        run p input |> Result.map (fun (v, input) -> (f v, input))
+
+    { parse = inner; label = "unknown" }
+
+let returnP (x : 'a) : Parser<'a> =
+    let inner input = Ok (x, input)
+
+    { parse = inner; label = "unknown" }
+
+let applyP (fp : Parser<'a -> 'b>) (xp : Parser<'a>) : Parser<'b> = bind (fun f x -> f x) fp xp
+
+let ( .>>. ) p1 p2 = bind (fun a b -> (a,b)) p1 p2
+let ( <|> ) p1 p2 = alt p1 p2
+let ( <!> ) = mapP
+let ( |>> ) x f = mapP f x
+let ( <*> ) = applyP
+
+let rec private zeroOrMoreInner (p : Parser<'a>) input =
+    match run p input with
+    | Ok (value, input) ->
+        let (values, input) = zeroOrMoreInner p input
+        (value::values, input)
+    | Error _ -> ([], input)
+
+let zeroOrMore (p : Parser<'a>) : (Parser<'a list>) =
+    let inner = zeroOrMoreInner p >> Result.Ok
+
+    { parse = inner; label = "unknown" }
+
+let oneOrMore (p : Parser<'a>) : (Parser<'a list>) =
+    let inner input =
+        run p input |> Result.map (fun (value, input) ->
+            let (values, input) = zeroOrMoreInner p input
+            (value::values, input))
+
+    { parse = inner; label = "unknown" }
+
+
+let oneOrMoreWithSep (p : Parser<'a>) (s : Parser<'b>) : (Parser<'a list>) =
+    let inner input =
+        run p input |> Result.map (fun (value, input) ->
+            let (values, input) = zeroOrMoreInner (bind (fun _ b -> b) s p) input
+            (value::values, input))
+
+    { parse = inner; label = "unknown" }
+
--- FuncComp.Parsing/Parser.fs
+++ FuncComp.Parsing/Parser.fs
@@ -9,62 +9,69 @@ let private tokenValue token = match token with
                                | Token.Identifier s -> s
                                | Token.Number n -> n |> string
                                | Token.Other s -> s
-let private lit value = sat (fun t -> value = (tokenValue t))
-let private var = sat (fun t -> match t with | Identifier ident -> keywords.Contains ident |> not | _ -> false) |> apply <| (tokenValue >> Name)
-let private num = sat (fun t -> match t with | Number _ -> true | _ -> false) |> apply <| fun t -> match t with | Number value -> value | _ -> failwith "invalid"
+let private lit value = sat (fun t -> value = (tokenValue t)) ("literal: " + value)
+let private var = sat (fun t -> match t with | Identifier ident -> keywords.Contains ident |> not | _ -> false) "variable" |>> (tokenValue >> Name)
+let private num = sat (fun t -> match t with | Number _ -> true | _ -> false) "number" |>> fun t -> match t with | Number value -> value | _ -> failwith "invalid"
 
-let private tokenToVar token = tokenValue token |> Name |> Expression<Name>.Variable
+let private strToVar str = str |> Name |> Expression<Name>.Variable
+let private tokenToVar token = tokenValue token |> strToVar
 
-let private  relop = altM [lit "<"; lit "<="; lit "=="; lit "~="; lit ">="; lit ">" ]
-let private apReduce exprs : Expression<Name> = Seq.reduce (fun a b -> upcast Expression<Name>.Application (a, b)) exprs
+let private expression, expressionRef = createParserForwardedToRef<Expression<Name>>()
 
 let private alternativeTag = bind3 (fun _ tag _ -> tag) (lit "<") num (lit ">")
+let private alternative = bind4 (fun tag parameters _ body -> Alternative<Name> (tag, parameters |> Seq.toList, body)) alternativeTag (zeroOrMore var) (lit "->") expression
+let private alternatives = oneOrMoreWithSep alternative (lit ";")
 
-let rec private expression tokens : (Expression<Name> * Token list) seq = tokens |> altM [
+let private definition = bind3 (fun name _ expr -> struct (name, expr)) var (lit "=") expression
+let private definitions = oneOrMoreWithSep definition (lit ";")
+
+let private atomicExpression : Parser<Expression<Name>> = choice [
+        var |>> (fun n -> upcast Expression<Name>.Variable n)
+        num |>> (fun n -> upcast Expression<Name>.Number n)
+        // TODO Pack { num , num }
+        bind3 (fun _ expr _ -> expr) (lit "(") expression (lit ")")
+    ]
+
+let private apReduce exprs : Expression<Name> = Seq.reduce (fun a b -> upcast Expression<Name>.Application (a, b)) exprs
+let private expression6 : Parser<Expression<Name>> = (oneOrMore atomicExpression) |>> apReduce
+
+let private binOpReduce op exprs : Expression<Name> = Seq.reduce (fun a e -> upcast Expression<Name>.Application (Expression<Name>.Application (op |> strToVar , a), e)) exprs
+
+let private expression5 : Parser<Expression<Name>> = choice [
+        bind3 (fun l op r -> binOpReduce (op |> tokenValue) [l; r]) expression6 (lit "/") expression6
+        oneOrMoreWithSep expression6 (lit "*") |>> binOpReduce("*")
+    ]
+
+let private expression4 : Parser<Expression<Name>> = choice [
+        bind3 (fun l op r -> binOpReduce (op |> tokenValue) [l; r]) expression5 (lit "-") expression5
+        oneOrMoreWithSep expression5 (lit "+") |>> binOpReduce("+")
+    ]
+
+let private  relop = choice [lit "<"; lit "<="; lit "=="; lit "~="; lit ">="; lit ">" ]
+let private expression3 : Parser<Expression<Name>> = choice [
+        bind3 (fun l op r -> binOpReduce (op |> tokenValue) [l; r]) expression5 (relop) expression5
+        expression4
+    ]
+
+let private expression2 : Parser<Expression<Name>> = oneOrMoreWithSep expression3 (lit "&") |>> binOpReduce("&")
+
+let private expression1 : Parser<Expression<Name>> = oneOrMoreWithSep expression2 (lit "|") |>> binOpReduce("|")
+
+let private expressionActual : Parser<Expression<Name>> = choice [
         bind4 (fun _ defs _ expr -> upcast Expression<Name>.Let (false, defs |> Seq.toList, expr)) (lit "let") definitions (lit "in") expression
         bind4 (fun _ defs _ expr -> upcast Expression<Name>.Let (true, defs |> Seq.toList, expr)) (lit "letrec") definitions (lit "in") expression
         bind4 (fun _ expr _ alts -> upcast Expression<Name>.Case (expr, alts |> Seq.toList)) (lit "case") expression (lit "of") alternatives
         expression1
     ]
-    and private expression1 tokens : (Expression<Name> * Token list) seq = tokens |> altM [
-        bind3 (fun l op r -> upcast Expression<Name>.Application (Expression<Name>.Application (op |> tokenToVar, l), r)) expression2 (lit "|") expression1
-        expression2
-    ]
-    and private expression2 tokens : (Expression<Name> * Token list) seq = tokens |> altM [
-        bind3 (fun l op r -> upcast Expression<Name>.Application (Expression<Name>.Application (op |> tokenToVar, l), r)) expression3 (lit "&") expression2
-        expression3
-    ]
-    and private expression3 tokens : (Expression<Name> * Token list) seq = tokens |> altM [
-        bind3 (fun l op r -> upcast Expression<Name>.Application (Expression<Name>.Application (op |> tokenToVar, l), r)) expression4 (relop) expression4
-        expression4
-    ]
-    and private expression4 tokens : (Expression<Name> * Token list) seq = tokens |> altM [
-        bind3 (fun l op r -> upcast Expression<Name>.Application (Expression<Name>.Application (op |> tokenToVar, l), r)) expression5 (lit "+") expression4
-        bind3 (fun l op r -> upcast Expression<Name>.Application (Expression<Name>.Application (op |> tokenToVar, l), r)) expression5 (lit "-") expression5
-        expression5
-    ]
-    and private expression5 tokens : (Expression<Name> * Token list) seq = tokens |> altM [
-        bind3 (fun l op r -> upcast Expression<Name>.Application (Expression<Name>.Application (op |> tokenToVar, l), r)) expression6 (lit "*") expression5
-        bind3 (fun l op r -> upcast Expression<Name>.Application (Expression<Name>.Application (op |> tokenToVar, l), r)) expression6 (lit "/") expression6
-        expression6
-    ]
-    and private expression6 tokens : (Expression<Name> * Token list) seq = tokens |> apply (oneOrMore atomicExpression) apReduce
-    and private atomicExpression tokens : (Expression<Name> * Token list) seq = tokens |> altM [
-        apply var (fun n -> upcast Expression<Name>.Variable n)
-        apply num (fun n -> upcast Expression<Name>.Number n)
-        // TODO Pack { num , num }
-        bind3 (fun _ expr _ -> expr) (lit "(") expression (lit ")")
-    ]
-    and private alternative = bind4 (fun tag parameters _ body -> Alternative<Name> (tag, parameters |> Seq.toList, body)) alternativeTag (zeroOrMore var) (lit "->") expression
-    and private alternatives = oneOrMoreWithSep alternative (lit ";")
-    and private definition = bind3 (fun name _ expr -> struct (name, expr)) var (lit "=") expression
-    and private definitions = oneOrMoreWithSep definition (lit ";")
-
+expressionRef := expressionActual
 
 let private supercombinatorDefinition : Parser<SupercombinatorDefinition<Name>> =
     bind4 (fun a b _ c -> SupercombinatorDefinition<Name> (a, b |> Seq.toList, c)) var (zeroOrMore var) (lit "=") expression
 
-let private program = apply (oneOrMoreWithSep supercombinatorDefinition (lit ";")) (fun scDefs -> Program<Name> (scDefs |> Seq.toList))
+let private program = oneOrMoreWithSep supercombinatorDefinition (lit ";") |>> (fun scDefs -> Program<Name> (scDefs |> Seq.toList))
 
 let parse (tokens : Token seq) : Program<Name> =
-    program (tokens |> Seq.toList) |> Seq.filter (fun (_, remaining) -> List.isEmpty remaining) |> Seq.head |> fst
+    match run program (tokens |> Seq.toList) with
+    | Ok (result, []) -> result
+    | Ok (_, tokens) -> failwithf "didn't consume all the tokens: %O" tokens
+    | Error (source, message) -> failwithf "%s: %s" source message
